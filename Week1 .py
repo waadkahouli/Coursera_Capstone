6d3f11049d98b97a87be6e9172d9104b7f79fa53
waadkahouli <64476794+waadkahouli@users.noreply.github.com>
2020-04-28 07:29:54 -0700

    Add files via upload

---
+++ Week1 .py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> This file is part of the capstone project 
+
+# In[2]:
+
+
+import pandas as pd 
+import numpy as np
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
